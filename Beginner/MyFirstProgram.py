6959d113fb67ec053f3e74d195210ca28d246c5c
CHINONSO ORJI <chinonosandorji@gmail.com>
2025-01-04 16:07:02 +0100

    my first python program

---
+++ Beginner/MyFirstProgram.py
@@ -0,0 +1,11 @@
+'''
+This program ask the user for name and age
+'''
+# step 1: ask for the users name
+name = input("Hello dear, please what is your name: ")
+
+# step 2: ask for the users age
+age = input(("Hello " + name + " how old are you? "))
+
+# Display the user data
+print("your name is " + name + " and you are " + age + " years old")
